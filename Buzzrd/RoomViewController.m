3b696b6cdeef21ed9fa7b73e27652d560d83856e
bmancini55 <bmancini@gmail.com>
2014-05-18 19:51:59 -0700

    scroll to bottom by default

--- Buzzrd/RoomViewController.m
+++ Buzzrd/RoomViewController.m
@@ -42,6 +42,16 @@
     CGRect tableFrame = self.tableView.frame;
     tableFrame.size.height = tableFrame.size.height - 40;
     self.tableView.frame = tableFrame;
+    
+    // create hooks for keyboard
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(onKeyboardOpen:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(onKeyboardClose:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
 }
 
 #pragma mark - Internal helper methods
@@ -60,8 +70,7 @@
         // scroll to bottom if this was first page
         if (page == 1)
         {
-            CGPoint bottomOffset = CGPointMake(0, self.tableView.contentSize.height - self.tableView.bounds.size.height);
-            [self.tableView setContentOffset:bottomOffset animated:false];
+            [self scrollToBottom:false];
         }
     };
     
@@ -75,6 +84,12 @@
                                                     page:page
                                                  success:success
                                                  failure:failure];
+}
+
+- (void)scrollToBottom:(BOOL)animated
+{
+    CGPoint bottomOffset = CGPointMake(0, self.tableView.contentSize.height - self.tableView.bounds.size.height);
+    [self.tableView setContentOffset:bottomOffset animated:animated];
 }
 
 
@@ -141,6 +156,8 @@
     [self.tableView beginUpdates];
     [self.tableView insertRowsAtIndexPaths:@[path] withRowAnimation:UITableViewRowAnimationAutomatic];
     [self.tableView endUpdates];
+    
+    [self scrollToBottom:false];
 }
 
 
@@ -166,6 +183,42 @@
         NSString *message = packet.args[0];
         [self receiveMessage:message];
     }            
+}
+
+
+#pragma mark - Keyboard methods
+
+-(void)onKeyboardOpen:(NSNotification *)notification
+{
+    // This code will move the keyboard
+    NSDictionary *userInfo = [notification userInfo];
+    NSTimeInterval animationDuration;
+    UIViewAnimationCurve animationCurve;
+    CGRect keyboardEndFrame;
+    
+    [[userInfo objectForKey:UIKeyboardAnimationCurveUserInfoKey] getValue:&animationCurve];
+    [[userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey] getValue:&animationDuration];
+    [[userInfo objectForKey:UIKeyboardFrameEndUserInfoKey] getValue:&keyboardEndFrame];
+    
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:animationDuration];
+    [UIView setAnimationCurve:animationCurve];
+    
+    CGRect newFrame = self.tableView.frame;
+    newFrame.origin.y = keyboardEndFrame.origin.y - newFrame.size.height - 40;
+    self.tableView.frame = newFrame;
+    
+    [UIView commitAnimations];
+}
+
+-(void)onKeyboardClose:(NSNotification *)notification
+{
+    NSDictionary *keyboardInfo = [notification userInfo];
+    NSValue *keyboardFrameEnd = [keyboardInfo valueForKey:UIKeyboardFrameEndUserInfoKey];
+    CGRect keyboardFrameEndRect = [keyboardFrameEnd CGRectValue];
+    
+    CGRect newFrame = self.tableView.frame;
+    newFrame.origin.y = keyboardFrameEndRect.origin.y - newFrame.size.height - 40;
 }
 
 
