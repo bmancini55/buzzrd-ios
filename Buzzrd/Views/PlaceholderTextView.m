b2091c024805f935a85b0fe8323e74a275b91c20
bmancini55 <bmancini@gmail.com>
2014-08-09 20:22:34 -0400

    fixing placeholder nil check

--- Buzzrd/Views/PlaceholderTextView.m
+++ Buzzrd/Views/PlaceholderTextView.m
@@ -46,21 +46,23 @@
 - (void)drawRect:(CGRect)rect
 {
     if(self.placeholder.length > 0) {
-
-        float linePadding = self.textContainer.lineFragmentPadding;
-        CGRect placeholderRect = CGRectMake(self.textContainerInset.left + linePadding,
-                                            self.textContainerInset.top,
-                                            rect.size.width - self.textContainerInset.left - self.textContainerInset.right - 2 * linePadding,
-                                            rect.size.height - self.textContainerInset.top - self.textContainerInset.bottom);
-        self.placeholderLabel = [[UILabel alloc]initWithFrame:placeholderRect];
-        self.placeholderLabel.font = self.font;
-        self.placeholderLabel.textAlignment = self.textAlignment;
-        self.placeholderLabel.textColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.3];
-        [self addSubview:self.placeholderLabel];
+        if(self.placeholderLabel == nil) {
+            
+            float linePadding = self.textContainer.lineFragmentPadding;
+            CGRect placeholderRect = CGRectMake(self.textContainerInset.left + linePadding,
+                                                self.textContainerInset.top,
+                                                rect.size.width - self.textContainerInset.left - self.textContainerInset.right - 2 * linePadding,
+                                                rect.size.height - self.textContainerInset.top - self.textContainerInset.bottom);
+            self.placeholderLabel = [[UILabel alloc]initWithFrame:placeholderRect];
+            self.placeholderLabel.font = self.font;
+            self.placeholderLabel.textAlignment = self.textAlignment;
+            self.placeholderLabel.textColor = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.3];
+            [self addSubview:self.placeholderLabel];
+            
+            self.placeholderLabel.text = self.placeholder;
+            [self.placeholderLabel sizeToFit];
         
-        self.placeholderLabel.text = self.placeholder;
-        [self.placeholderLabel sizeToFit];
-        
+        }
         [self showOrHidePlaceholder];
     }
     
