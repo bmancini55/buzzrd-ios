e973e4bb111683b30be16431a566def68c00813a
bmancini55 <bmancini@gmail.com>
2014-09-18 19:02:03 -0400

    Basic styling for rooms screen

--- Buzzrd/Views/Room/RoomCell.m
+++ Buzzrd/Views/Room/RoomCell.m
@@ -12,7 +12,6 @@
 @interface RoomCell()
 
 @property (strong, nonatomic) UILabel *nameLabel;
-@property (strong, nonatomic) UILabel *defaultLabel;
 @property (strong, nonatomic) UILabel *venueLabel;
 @property (strong, nonatomic) UILabel *addressLabel;
 @property (strong, nonatomic) UILabel *userCountLabel;
@@ -40,16 +39,10 @@
     self.backgroundColor = [ThemeManager getPrimaryColorLight];
     
     self.nameLabel = [[UILabel alloc]init];
-    self.nameLabel.font = [ThemeManager getPrimaryFontDemiBold:14.0];
+    self.nameLabel.font = [ThemeManager getPrimaryFontRegular:17.0f];
     self.nameLabel.textColor = [ThemeManager getPrimaryColorDark];
     self.nameLabel.translatesAutoresizingMaskIntoConstraints = NO;
     [self.contentView addSubview:self.nameLabel];
-    
-    self.defaultLabel = [[UILabel alloc]init];
-    self.defaultLabel.font = [ThemeManager getPrimaryFontRegular:9.0];
-    self.defaultLabel.textColor = [ThemeManager getPrimaryColorDark];
-    self.defaultLabel.translatesAutoresizingMaskIntoConstraints = NO;
-    [self.contentView addSubview:self.defaultLabel];
     
     self.venueLabel = [[UILabel alloc] init];
     self.venueLabel.font = [ThemeManager getPrimaryFontRegular:12.0f];
@@ -64,19 +57,19 @@
     [self.contentView addSubview:self.addressLabel];
     
     self.messageCountLabel = [[UILabel alloc]init];
-    self.messageCountLabel.font = [ThemeManager getPrimaryFontBold:10.0];
-    self.messageCountLabel.textColor = [ThemeManager getPrimaryColorDark];
+    self.messageCountLabel.font = [ThemeManager getPrimaryFontRegular:28.0];
+    self.messageCountLabel.textColor = [ThemeManager getTertiaryColorDark];
     self.messageCountLabel.translatesAutoresizingMaskIntoConstraints = NO;
     [self.contentView addSubview:self.messageCountLabel];
 
     self.messageLabel = [[UILabel alloc]init];
-    self.messageLabel.font = [ThemeManager getPrimaryFontMedium:10.0];
+    self.messageLabel.font = [ThemeManager getPrimaryFontMedium:12.0];
     self.messageLabel.textColor = [ThemeManager getPrimaryColorDark];
     self.messageLabel.translatesAutoresizingMaskIntoConstraints = NO;
     [self.contentView addSubview:self.messageLabel];
     
     self.distanceLabel = [[UILabel alloc] init];
-    self.distanceLabel.font = [ThemeManager getPrimaryFontRegular:11.0];
+    self.distanceLabel.font = [ThemeManager getPrimaryFontDemiBold:11.0];
     self.distanceLabel.textColor = [ThemeManager getPrimaryColorDark];
     self.distanceLabel.textAlignment = NSTextAlignmentRight;
     self.distanceLabel.translatesAutoresizingMaskIntoConstraints = NO;
@@ -88,7 +81,6 @@
     NSDictionary *views =
         @{
             @"title": self.nameLabel,
-            @"default": self.defaultLabel,
             @"venue": self.venueLabel,
             @"distance": self.distanceLabel,
             @"address": self.addressLabel,
@@ -97,17 +89,18 @@
         };
     
     [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-12-[title]" options:0 metrics:nil views:views]];
-    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[title]-3-[default]" options:NSLayoutFormatAlignAllBaseline metrics:nil views:views]];
-    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[address]-(>=6)-[distance]" options:NSLayoutFormatAlignAllBottom metrics:nil views:views]];
-    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[distance]-12-|" options:0 metrics:nil views:views]];
-    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[title]-(>=6)-[msgCount]" options:NSLayoutFormatAlignAllBaseline metrics:nil views:views]];
-    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[msgCount]-3-[msg]" options:NSLayoutFormatAlignAllBaseline metrics:nil views:views]];
-    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[msg]-12-|" options:0 metrics:nil views:views]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[title]-(>=6)-[msgCount]" options:0 metrics:nil views:views]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[msgCount]-12-|" options:0 metrics:nil views:views]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[venue]-6-[distance]" options:NSLayoutFormatAlignAllBaseline metrics:nil views:views]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[distance]-(>=12)-[msgCount]" options:0 metrics:nil views:views]];
     
     [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-6-[title]" options:0 metrics:nil views:views]];
-    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[title]-(0)-[venue]" options:NSLayoutFormatAlignAllLeft metrics:nil views:views]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[title]-(-3)-[venue]" options:NSLayoutFormatAlignAllLeft metrics:nil views:views]];
     [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[venue]-(-1)-[address]" options:NSLayoutFormatAlignAllLeft metrics:nil views:views]];
     [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[address]-6-|" options:0 metrics:nil views:views]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-6-[msgCount]" options:0 metrics:nil views:views]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[msgCount]-(-12)-[msg]" options:NSLayoutFormatAlignAllRight metrics:nil views:views]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[msg]-(>=6)-|" options:0 metrics:nil views:views]];
     
     
     [super updateConstraints];
@@ -119,27 +112,19 @@
     
     self.nameLabel.text = room.name;
     
-    if(room.venueDefault) {
-        self.defaultLabel.text = [NSString stringWithFormat:@"(%@)", NSLocalizedString(@"Default", nil)];
-        self.defaultLabel.hidden = false;
-    } else {
-        self.defaultLabel.text = @"";
-        self.defaultLabel.hidden = true;
-    }
-    
     self.venueLabel.text = room.venue.name;
     self.addressLabel.text = [room.venue.location prettyString];
     
-    self.messageCountLabel.text = [NSString stringWithFormat:@"%lu", (unsigned long)room.messageCount];
+    self.messageCountLabel.text = [self formatMessages:(int)room.messageCount];
     self.messageLabel.text = NSLocalizedString(@"msgs", nil);
     
     // set distance
     CLLocationDistance distance = [userLocation distanceFromLocation:room.location];
     CGFloat distanceInFeet = distance / 1609.344 * 5280;
     if(distanceInFeet < 500)
-        self.distanceLabel.text = [NSString stringWithFormat:@"%1.f ft", distanceInFeet];
+        self.distanceLabel.text = [NSString stringWithFormat:@"(%1.f ft)", distanceInFeet];
     else
-        self.distanceLabel.text = [NSString stringWithFormat:@"%.1f mi", distanceInFeet / 5280];
+        self.distanceLabel.text = [NSString stringWithFormat:@"(%.1f mi)", distanceInFeet / 5280];
     
     [self updateConstraints];
     [self addBorder];
@@ -153,7 +138,7 @@
     // create on new
     if(self.bottomBorder == nil) {
         self.bottomBorder = [CALayer layer];
-        self.bottomBorder.backgroundColor = [ThemeManager getTertiaryColorDark].CGColor;
+        self.bottomBorder.backgroundColor = [ThemeManager getSecondaryColorDark].CGColor;
         [self.layer addSublayer:self.bottomBorder];
     }
     
@@ -168,5 +153,25 @@
     return contentHeight + borderWidth;
 }  // Configure the view for the selected state
 
+
+- (NSString *)formatMessages:(int)messageCount
+{
+    NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+    NSNumber *messageNumber = [NSNumber numberWithInt:messageCount];
+    
+    if(messageCount < 1000) {
+        [numberFormatter setPositiveFormat:@"###0"];
+    }
+    else if (messageCount < 10000) {
+        [numberFormatter setPositiveFormat:@"0.0k"];
+        messageNumber = [NSNumber numberWithFloat:[messageNumber floatValue] / 1000.0];
+    }
+    else if (messageCount < 1000000) {
+        [numberFormatter setPositiveFormat:@"###0k"];
+        messageNumber = [NSNumber numberWithFloat:[messageNumber floatValue] / 1000.0];
+    }
+    
+    return [numberFormatter stringFromNumber:messageNumber];
+}
 
 @end
