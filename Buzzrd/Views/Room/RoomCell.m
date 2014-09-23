d1f9ac33df524fb24c04ec9665725b756864c894
bmancini55 <bmancini@gmail.com>
2014-09-23 08:40:49 -0400

    Completed styling of rooms list

--- Buzzrd/Views/Room/RoomCell.m
+++ Buzzrd/Views/Room/RoomCell.m
@@ -20,6 +20,7 @@
 @property (strong, nonatomic) UILabel *messageLabel;
 @property (strong, nonatomic) UILabel *distanceLabel;
 @property (strong, nonatomic) CALayer *bottomBorder;
+@property (strong, nonatomic) UIImageView *typeImage;
 
 @end
 
@@ -45,7 +46,7 @@
     [self.contentView addSubview:self.nameLabel];
     
     self.venueLabel = [[UILabel alloc] init];
-    self.venueLabel.font = [ThemeManager getPrimaryFontRegular:12.0f];
+    self.venueLabel.font = [ThemeManager getPrimaryFontBold:10.0f];
     self.venueLabel.textColor = [ThemeManager getPrimaryColorDark];
     self.venueLabel.translatesAutoresizingMaskIntoConstraints = NO;
     [self.contentView addSubview:self.venueLabel];
@@ -57,23 +58,27 @@
     [self.contentView addSubview:self.addressLabel];
     
     self.messageCountLabel = [[UILabel alloc]init];
-    self.messageCountLabel.font = [ThemeManager getPrimaryFontRegular:28.0];
+    self.messageCountLabel.font = [ThemeManager getPrimaryFontBold:17.0];
     self.messageCountLabel.textColor = [ThemeManager getTertiaryColorDark];
     self.messageCountLabel.translatesAutoresizingMaskIntoConstraints = NO;
     [self.contentView addSubview:self.messageCountLabel];
 
     self.messageLabel = [[UILabel alloc]init];
-    self.messageLabel.font = [ThemeManager getPrimaryFontMedium:12.0];
+    self.messageLabel.font = [ThemeManager getPrimaryFontRegular:10.0];
     self.messageLabel.textColor = [ThemeManager getPrimaryColorDark];
     self.messageLabel.translatesAutoresizingMaskIntoConstraints = NO;
     [self.contentView addSubview:self.messageLabel];
     
     self.distanceLabel = [[UILabel alloc] init];
-    self.distanceLabel.font = [ThemeManager getPrimaryFontDemiBold:11.0];
+    self.distanceLabel.font = [ThemeManager getPrimaryFontMedium:9.0];
     self.distanceLabel.textColor = [ThemeManager getPrimaryColorDark];
     self.distanceLabel.textAlignment = NSTextAlignmentRight;
     self.distanceLabel.translatesAutoresizingMaskIntoConstraints = NO;
     [self.contentView addSubview:self.distanceLabel];
+    
+    self.typeImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Venue.png"]];
+    self.typeImage.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.contentView addSubview:self.typeImage];
 }
 
 - (void) updateConstraints
@@ -85,23 +90,29 @@
             @"distance": self.distanceLabel,
             @"address": self.addressLabel,
             @"msgCount": self.messageCountLabel,
-            @"msg": self.messageLabel
+            @"msg": self.messageLabel,
+            @"image": self.typeImage
         };
     
     [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-12-[title]" options:0 metrics:nil views:views]];
     [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[title]-(>=6)-[msgCount]" options:0 metrics:nil views:views]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[title]-(>=6)-[msg]" options:0 metrics:nil views:views]];
     [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[msgCount]-12-|" options:0 metrics:nil views:views]];
-    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[venue]-6-[distance]" options:NSLayoutFormatAlignAllBaseline metrics:nil views:views]];
-    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[distance]-(>=12)-[msgCount]" options:0 metrics:nil views:views]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-12-[image(17)]" options:0 metrics:nil views:views]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[image]-6-[venue]-(>=6)-|" options:0 metrics:nil views:views]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[image]-6-[address]" options:0 metrics:nil views:views]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[address]-(>=6)-[distance]" options:NSLayoutFormatAlignAllBaseline metrics:nil views:views]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[distance]-12-|" options:0 metrics:nil views:views]];
     
     [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-6-[title]" options:0 metrics:nil views:views]];
-    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[title]-(-3)-[venue]" options:NSLayoutFormatAlignAllLeft metrics:nil views:views]];
-    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[venue]-(-1)-[address]" options:NSLayoutFormatAlignAllLeft metrics:nil views:views]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[title]-(0)-[image(22)]" options:0 metrics:nil views:views]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[title]-(-1)-[venue]-(-1)-[address]" options:0 metrics:nil views:views]];
     [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[address]-6-|" options:0 metrics:nil views:views]];
-    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-6-[msgCount]" options:0 metrics:nil views:views]];
-    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[msgCount]-(-12)-[msg]" options:NSLayoutFormatAlignAllRight metrics:nil views:views]];
-    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[msg]-(>=6)-|" options:0 metrics:nil views:views]];
     
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-6-[msgCount]" options:0 metrics:nil views:views]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[msgCount]-(-9)-[msg]" options:NSLayoutFormatAlignAllRight metrics:nil views:views]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[msg]-(>=6)-[distance]" options:0 metrics:nil views:views]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[distance]-6-|" options:0 metrics:nil views:views]];
     
     [super updateConstraints];
 }
@@ -111,9 +122,6 @@
     _room = room;
     
     self.nameLabel.text = room.name;
-    
-    self.venueLabel.text = room.venue.name;
-    self.addressLabel.text = [room.venue.location prettyString];
     
     self.messageCountLabel.text = [self formatMessages:(int)room.messageCount];
     self.messageLabel.text = NSLocalizedString(@"msgs", nil);
@@ -125,6 +133,17 @@
         self.distanceLabel.text = [NSString stringWithFormat:@"(%1.f ft)", distanceInFeet];
     else
         self.distanceLabel.text = [NSString stringWithFormat:@"(%.1f mi)", distanceInFeet / 5280];
+    
+    // set type information
+    if(room.venue) {
+        self.typeImage.image = [UIImage imageNamed:@"Venue.png"];
+        self.venueLabel.text = room.venue.name;
+        self.addressLabel.text = [room.venue.location prettyString];
+    } else {
+        self.typeImage.image = [UIImage imageNamed:@"Pinpoint.png"];
+        self.venueLabel.text = @"Unknown Location";
+        self.addressLabel.text = @"Pittsburgh, PA";
+    }
     
     [self updateConstraints];
     [self addBorder];
