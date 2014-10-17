148e068811036d5444bf063dd715f342501f51e0
rsbst23 <rsbst23@yahoo.com>
2014-10-16 23:10:47 -0400

    Changed Nearby Rooms text to Nearby

--- Buzzrd/Controllers/NearbyRoomsViewController.m
+++ Buzzrd/Controllers/NearbyRoomsViewController.m
@@ -24,9 +24,9 @@
 {
     [super loadView];
     
-    self.title = NSLocalizedString(@"nearby_rooms", nil);
-    self.navigationController.title = [NSLocalizedString(@"nearby_rooms", nil) uppercaseString];
-    self.sectionHeaderTitle = [NSLocalizedString(@"nearby_rooms", nil) uppercaseString];
+    self.title = NSLocalizedString(@"nearby", nil);
+    self.navigationController.title = [NSLocalizedString(@"nearby", nil) uppercaseString];
+    self.sectionHeaderTitle = [NSLocalizedString(@"nearby", nil) uppercaseString];
     self.emptyNote = NSLocalizedString(@"nearbyrooms_empty_note", nil);
     
     UISearchBar *searchBar = [[UISearchBar alloc]init];
