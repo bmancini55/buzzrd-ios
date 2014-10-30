4c6e08f658b202fae43b2d61657ca6635181ceae
rsbst23 <rsbst23@yahoo.com>
2014-10-30 01:01:11 -0400

    Hide the header section on the Room Actions Controller

--- Buzzrd/Controllers/RoomActionsViewController.m
+++ Buzzrd/Controllers/RoomActionsViewController.m
@@ -113,7 +113,7 @@
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
-    return 30.0;
+    return 0.0;
 }
 
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
